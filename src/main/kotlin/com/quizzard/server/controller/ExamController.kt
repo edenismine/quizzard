a125e664fbdc20b15079280fbd68560bab278130
Daniel Aragon <daniel.aragon@ciencias.unam.mx>
2018-09-08 16:54:22 -0500

    Corrected request parameters for create operation

--- src/main/kotlin/com/quizzard/server/controller/ExamController.kt
+++ src/main/kotlin/com/quizzard/server/controller/ExamController.kt
@@ -1,23 +1,23 @@
 package com.quizzard.server.controller
 
 import com.quizzard.server.domain.Exam
+import com.quizzard.server.domain.Question
 import com.quizzard.server.repository.ExamRepository
 import org.springframework.web.bind.annotation.*
 import java.util.*
 
 @RestController
 class ExamController(val repository: ExamRepository) {
-
     @RequestMapping("/api/exam", method = [RequestMethod.GET])
     fun findAll(): List<Exam> = repository.findAll()
 
     @RequestMapping("/api/exam", method = [RequestMethod.POST])
-    fun new(@RequestBody exam: Exam) = repository.save(exam)
+    fun new(@RequestParam name: String, @RequestParam questions: List<Question>) =
+            repository.save(Exam(name = name, questions = questions))
 
     @RequestMapping("/api/exam/{id}", method = [RequestMethod.PUT])
     fun update(@RequestBody exam: Exam, @RequestParam id: UUID) = repository.save(exam.copy(id = id))
 
     @RequestMapping("/api/exam/{id}", method = [RequestMethod.DELETE])
     fun delete(@RequestParam id: UUID) = repository.deleteById(id)
-
 }
--- src/main/kotlin/com/quizzard/server/domain/Exam.kt
+++ src/main/kotlin/com/quizzard/server/domain/Exam.kt
@@ -5,7 +5,7 @@ import org.springframework.data.mongodb.core.mapping.Document
 import java.util.*
 
 @Document
-data class Exam(@Id val id: UUID, val name: String, val questions: List<Question>)
+data class Exam(@Id val id: UUID = UUID.randomUUID(), val name: String, val questions: List<Question>)
 
 data class Question(val id : Int, val question: String, val answers: List<Answer> )
 
